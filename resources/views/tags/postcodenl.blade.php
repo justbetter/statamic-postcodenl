db45d5c4c0ea87e5e1510f23d8965e40c846a3ee
Kevin Meijer <kevin@justbetter.nl>
2023-06-19 10:48:47 +0200

    Disable fields on load and empty on error

--- resources/views/tags/postcodenl.blade.php
+++ resources/views/tags/postcodenl.blade.php
@@ -29,6 +29,9 @@
                 return;
             }
 
+            streetElement.disabled = true;
+            cityElement.disabled = true;
+
             let xhr = new XMLHttpRequest();
             let data = {
                 postcode: zipcode,
@@ -40,18 +43,25 @@
             xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
             xhr.send(JSON.stringify(data));
             xhr.onload = function () {
+                streetElement.disabled = false;
+                cityElement.disabled = false;
+
                 if (xhr.readyState !== xhr.DONE) {
+                    streetElement.value = '';
+                    cityElement.value = '';
                     return;
                 }
 
                 let responseData = JSON.parse(xhr.response);
 
                 if (!responseData?.city || !responseData?.street) {
+                    streetElement.value = '';
+                    cityElement.value = '';
                     return;
                 }
 
-                streetElement.value = responseData.city
-                cityElement.value = responseData.street
+                streetElement.value = responseData.city;
+                cityElement.value = responseData.street;
             };
         }
     }
