066e4d21f0dcb1d651f158f8553995741a24cc60
Kevin Meijer <kev10@live.nl>
2023-06-19 09:36:43 +0200

    Fixed field updates showing wrong values

--- resources/views/tags/postcodenl.blade.php
+++ resources/views/tags/postcodenl.blade.php
@@ -50,8 +50,8 @@
                     return;
                 }
 
-                streetElement.value = responseData.city
-                cityElement.value = responseData.street
+                streetElement.value = responseData.street
+                cityElement.value = responseData.city
             };
         }
     }
